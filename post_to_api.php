55e1b05ff2ed4d31a22659819f8abe4cbca1a2bd
nbmartino <nbmartino@gmail.com>
2021-01-14 18:39:28 +0800

    Clean up outdated pseudocode.

--- post_to_api.php
+++ post_to_api.php
@@ -2,16 +2,8 @@
 
 include 'Api.php';
 include 'Data.php';
+
 session_start();
-
-
-// use email validator class
-// data stripper - against SQL injections
-// id generator
-//generate unique hexadcimal ID
-//echo 'alert("hi $first_name")';
-// Check if log-in form is submitted
-
 
 if (isset($_POST['login'])) {
 
